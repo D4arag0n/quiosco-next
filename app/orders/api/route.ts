0f12044fc27186a9217721c6891122caf05781a4
D4arag0n <lalo_19107@hotmail.com>
2026-03-06 11:22:09 -0600

    fix

--- app/orders/api/route.ts
+++ app/orders/api/route.ts
@@ -1,5 +1,7 @@
 import { prisma } from "@/src/lib/prisma";
 
+export const dynamic = 'force-dynamic'//Se agrega para que los endpoints sean dinamicos y no queden cacheados 
+//Y puedan actualizar la información
 
 export async function GET(){
     const orders = await prisma.order.findMany({
